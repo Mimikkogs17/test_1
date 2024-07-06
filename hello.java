687196671b2eed45170971a5a2688aca549f8912
Mimikkogs17 <gaomenghao34@gmail.com>
2024-07-06 11:06:53 +0800

    feat: :sparkles: hello3_1

--- hello.java
+++ hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main (String[] arr){
         System.out.println("hello world!");
         System.out.println("hello-2");
+        System.out.println("hello-3");
     }
 }
